76504dadbdd2565970354f433a8eaba4c735f36a
s12171934 <ssong99999@naver.com>
2024-02-05 16:21:20 +0900

    base-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <div id="user-input">
+        <div className="input-group">
+          <div>
+            <label>1111</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>1111</label>
+            <input type="number" />
+          </div>
+        </div>
+        <br />
+        <div className="input-group">
+          <div>
+            <label>1111</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>1111</label>
+            <input type="number" />
+          </div>
+        </div>
+      </div>
+      <table id="result">
+        <thead>
+          <th>1</th>
+          <th>1</th>
+          <th>1</th>
+          <th>1</th>
+          <th>1</th>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default App
+export default App;
